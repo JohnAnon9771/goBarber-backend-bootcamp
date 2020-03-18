01dfb2be3f5dff9a9864a2e617b6bf34074ec58e
JohnAnon9771 <njoao97710@gmail.com>
2020-03-17 22:09:41 -0300

    reparing files

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class Files extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `${process.env.APP_URL}/file${this.path}`;
+            return `${process.env.APP_URL}/files/${this.path}`;
           }
         }
       },
--- src/routes.js
+++ src/routes.js
@@ -34,7 +34,6 @@ routes.get('/schedules', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
-
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
